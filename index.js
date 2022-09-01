971720dc8a017c2c1926ad7228347f65f7d1a1cc
Jury Orava <jury.orava@yandex.ru>
2022-09-01 13:19:46 +0300

    popravil

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ let popup = document.querySelector('.popup');
 let profileEdit = document.querySelector('.profile__btn');
 let profileEditClose = document.querySelector('.popup__close');
 
+let profileName = document.querySelector('.profile__name-text');
+let profileDescription = document.querySelector('.profile__description');
+let profileEditSave = document.querySelector('.popup__btn');
+
+let newName = document.querySelectorAll('.popup__text')[0];
+let newDescription = document.querySelectorAll('.popup__text')[1];
+
 function popupOpened() {
     popup.classList.add('popup_opened');
 }
@@ -11,23 +18,16 @@ function popupClosed() {
     popup.classList.remove('popup_opened');
 }
 
-profileEdit.addEventListener('click', popupOpened);
-profileEditClose.addEventListener('click', popupClosed);
-
-let profileName = document.getElementById('name');
-let profileDescription = document.getElementById('description');
-let profileEditSave = document.querySelector('.popup__btn');
-
 function profileSave(evt) {
     evt.preventDefault();
-
-    let newName = document.getElementById('newName');
-    let newDescription = document.getElementById('newDescription');
 
     profileName.textContent = newName.value;
     profileDescription.textContent = newDescription.value;
 
-    popup.classList.remove('popup_opened');
+    popupClosed();
 }
 
-profileEditSave.addEventListener('click', profileSave);
+profileEdit.addEventListener('click', popupOpened);
+profileEditClose.addEventListener('click', popupClosed);
+
+profileEditSave.addEventListener('submit', profileSave);
