49e0386ff86a6b09d54e20fe9c2775cd005f3bfd
Jury Orava <jury.orava@yandex.ru>
2022-09-02 12:37:42 +0300

    moy popitka nomer pyat

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,16 @@ let profileEditClose = document.querySelector('.popup__close');
 
 let profileName = document.querySelector('.profile__name-text');
 let profileDescription = document.querySelector('.profile__description');
-let profileEditSave = document.querySelector('.popup__btn');
+let profileEditSave = document.querySelector('.form');
 
-let newName = document.querySelectorAll('.popup__text')[0];
-let newDescription = document.querySelectorAll('.popup__text')[1];
+let newName = document.querySelector('.popup__text');
+let newDescription = document.querySelector('.popup__text_mobile-margin_big');
 
 function popupOpened() {
     popup.classList.add('popup_opened');
+
+    newName.setAttribute('value', newName.value);
+    newDescription.setAttribute('value', newDescription.value);
 }
 
 function popupClosed() {
